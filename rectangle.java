66a64b32b174e569ddfbb3f0320b99fc4df10112
Nolan <NHorn25@users.noreply.github.com>
2015-10-10 22:54:12 -0500

    Create rectangle.java

---
+++ rectangle.java
@@ -0,0 +1,34 @@
+package nolan.s.shapes;
+
+public class rectangle extends shape implements Printable {
+
+// The height of the rectangle
+	private int height;
+// The width of the rectangle
+	private int width;
+
+	public rectangle(int height, int width) {
+		this.height = height;
+		this.width = width;
+// Invoke the methods in the superclass to set the type and name of this rectangle
+		setShapeType("quadrilateral");
+		setShapeName("rectangle");
+	}
+
+// Override the method inherited from Shape (which is implemented from the Printable interface) to handle custom description printing
+	@Override
+	public void printDescription() {
+// Invoke the super method because it contains the part that prints out the type, just for DRY purposes
+		super.printDescription();
+		System.out.println("Height is " + height + " units");
+		System.out.println("Width is " + width + " units");
+
+	}
+
+// Override the method inherited from Shape to calculate the area of a rectangle
+	@Override
+	public double getArea() {
+// Area of a rectangle is height * width
+		return height * width;
+	}
+}
